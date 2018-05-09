99a9e95f761c40c8f77bdfca1fb87e45af704ca9
libertyjoe <38118210+libertyjoe@users.noreply.github.com>
2018-05-08 21:49:04 -0400

    Add files via upload

--- HashTesting.java
+++ HashTesting.java
@@ -14,6 +14,33 @@ class HashTesting {
 
    public static void main(String [] args) throws IOException 
    {
+      int capacity = 241;
+      String firstName;
+      int keyNumber;
+   
+      File text = new File ("names.txt");
+      
+      if (text.exists()) {
+         System.out.println("File exists already - we can read it");
+      }else {
+         text.createNewFile();   
+      }
+
+      Scanner read = new Scanner(text);
+      
+      Table SingleHash = new Table(capacity);
+      
+      while(read.hasNext())
+      {
+         firstName = read.Next();
+         keyNumber = read.NextInt();
+         
+         //Next a couple things could be done
+         // test for key in use SingleHash.containsKey(keyNumber) or
+         // or use Put function, but I'm not sure how it works, why is there a return value of E? - Joe 5/8
+         
+         
+         
 
 
 
